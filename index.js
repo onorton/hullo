18e62b616d01a500e80d80ab2b3281793ccf792f
onorton <onorton125@gmail.com>
2017-02-04 23:37:22 +0000

    Still not working on physical alexa

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 'use strict';
 var Alexa = require("alexa-sdk");
-var appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.appId = appId;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 
 var handlers = {
+    
+    'LaunchRequest': function () {
+        this.emit(':ask', 'Talk to me.', 'Say something.');
+    },
     'ChatIntent': function() {
         var message = this.event.request.intent.slots.Message.value;
         var respond = function(message) {return 'what\'s up.'}
         var response = respond(message);
         console.log(message);
         console.log(response);
-            // With a callback, use the arrow function to preserve the correct 'this' context
-        this.emit(":ask", response);
+        
+	this.emit(":ask", response);
     },
     'SessionEndedRequest': function () {
         console.log('session ended!');

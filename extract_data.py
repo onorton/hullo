651b2bfd283adc13372ef6b3626fc185ce23d77f
onorton <onorton125@gmail.com>
2017-02-05 08:14:52 +0000

    Fixed REST
    Signed-off-by: onorton <onorton125@gmail.com>

--- extract_data.py
+++ extract_data.py
@@ -14,7 +14,7 @@ def extract_data(facebook, google):
     return join_data(data)
 
 def join_data(data):
-    return data[0]
+    return reduce(operator.add, [], data)
 
 
 def write_file(messages, filename):

42c740ba05cef1f7b5745c9786948eb3e5d232b5
Blaine Rogers <blaine.w.rogers@gmail.com>
2017-02-05 02:04:49 +0000

    Lots of stuff

--- ml.py
+++ ml.py
@@ -1,7 +1,12 @@
+from __future__ import print_function
+
 import tensorflow as tf
 from dnc.controller import BaseController
 from dnc.dnc import DNC
+import numpy as np
 import random
+import sys
+import json
 
 class FeedForwardController(BaseController):
     def network_vars(self):
@@ -16,11 +21,23 @@ class FeedForwardController(BaseController):
         activations = tf.nn.relu(output)
         return activations
 
-dnc = DNC(FeedForwardController, 8, 8, 10)
-optimizer = tf.train.RMSPropOptimizer(0.01, momentum=0.01)
+def binary_cross_entropy(predictions, targets):
+    return tf.reduce_mean(
+        -1 * targets * tf.log(predictions) - (1 - targets) * tf.log(1 - predictions)
+    )
 
-outputs, _ = dnc_instance.get_outputs()
-loss = tf.reduce_mean(tf.square(outputs - dnc_instance.target_output))
+with open('outfile.json', 'r') as f:
+    conversations = json.load(f)
+my_aliases = {'Blaine William Rogers', 'Blaine Rogers'}
+
+dnc = DNC(FeedForwardController, 10, 10, 1000)
+optimizer = tf.train.RMSPropOptimizer(0.001, momentum=0.5)
+
+outputs, _ = dnc.get_outputs()
+squeezed = tf.clip_by_value(tf.sigmoid(outputs), 1e-6, 1. - 1e-6)
+
+loss = binary_cross_entropy(squeezed, dnc.target_output)
+
 gradients = optimizer.compute_gradients(loss)
 apply_gradients = optimizer.apply_gradients(gradients)
 
@@ -28,24 +45,43 @@ def convert(n):
     bits = [int(digit) for digit in bin(n)[2:]]
     while len(bits) < 8:
         bits = [0] + bits
-    return bits
+    return bits + [0, 0]
 
 def deconvert(bits):
-    return int(''.join(str(b) for b in bits), base=2)
+    return int(''.join('0' if b < .5 else '1' for b in bits), base=2)
+
+def round_off(bits):
+    return map(lambda f: round(f, 2), bits)
 
 with tf.Session() as sess:
-    for step in range(100):
-        in_ = [random.randrange(24) for _ in range(random.randrange(10))]
-        out_ = [sum(in_)] + [0 for _ in range(len(in_) - 1)]
-        sess.run(apply_gradients, feed_dict={
-            dnc.input_data: list(map(convert, in_)),
-            dnc.target_output: list(map(convert, out_)),
-            dnc.sequence_length: len(in_),
-        })
-        print('.', end='')
-        if step == 99:
-            print()
-            print(in_, sess.run(outputs, {
-                dnc.input_data: list(map(convert, in_)),
-                dnc.sequence_length: len(in_),
-            }))
+    sess.run(tf.global_variables_initializer())
+    for conversation in conversations[0]:
+        for length in range(1, min(10, len(conversation['messages']) - 1)):
+            messages = conversation['messages'][:length]
+            in_ = []
+            for message in messages:
+                try:
+                    in_.extend(map(lambda c: convert(ord(c)), message['content']))
+                    in_.append([0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+                except:
+                    continue
+            in_.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+            out = map(lambda c: convert(ord(c)), conversation['messages'][length]['content'])
+            out += [[0] * 10] * 5
+            lenin = len(in_)
+            in_ += [[0] * 10] * len(out)
+            out = [[0] * 10] * lenin + out
+            assert len(in_) == len(out)
+
+            in_ = np.array([in_])
+            out = np.array([out])
+
+            o, l, _ = sess.run([squeezed, loss, apply_gradients], feed_dict={
+                dnc.input_data: in_,
+                dnc.target_output: out,
+                dnc.sequence_length: in_.shape[1],
+            })
+
+        print('loss', l, 'message', ''.join(map(lambda l: chr(deconvert(l[:-2])), o[0])), end='\n\n')
+
+    dnc.save(sess, 'checkpoints', 'checkpoint')
